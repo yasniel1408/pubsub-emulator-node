efdbbf05e398d31537a0060d041cf451cb0fb413
Yasniel <yasnielfajardoegues1408@gmail.com>
2023-12-14 18:03:46 -0300

    first commit

---
+++ index.mjs
@@ -0,0 +1,23 @@
+import express from "express";
+import { PubSub } from "@google-cloud/pubsub";
+
+const app = express();
+const port = 8000;
+
+const pubsub = new PubSub({
+  projectId: "localhost", // Asegúrate de usar el mismo proyecto que configuraste para el emulador
+  apiEndpoint: "http://localhost:8085", // La dirección y puerto del emulador
+});
+
+const subscription = pubsub.subscription("my-subscription");
+subscription.on("message", (message) => {
+  console.log(`Nuevo mensaje: ${message.data}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Servidor en ejecución");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${port}`);
+});
